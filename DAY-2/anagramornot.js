7200133049e4eaf976cc61980fdc13c70d462fed
Mekala Jyothsna <100584592+Jyothsnajyothsna@users.noreply.github.com>
2023-05-17 10:30:20 +0530

    Delete DAY-2 directory

--- DAY-2/anagramornot.js
+++
@@ -1,8 +0,0 @@
-function areAnagrams(string1, string2) {
-    const sortedString1 = string1.toLowerCase().split('').sort().join('');
-    const sortedString2 = string2.toLowerCase().split('').sort().join('');
-  
-    return sortedString1 === sortedString2;
-  }
-  console.log(areAnagrams("study", "dusty")); 
-  
